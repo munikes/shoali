995f8b27611677235f841f58a0e15059b197617a
mUniKeS <munikes@members.fsf.org>
2013-04-03 18:23:26 +0200

    Put localhost field in setting.py, that is required in PostgreSQL

--- shoali_django/main/settings.py
+++ shoali_django/main/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'shoali',          # Or path to database file if using sqlite3.
         'USER': 'shoali',          # Not used with sqlite3.
         'PASSWORD': 'shoali321',   # Not used with sqlite3.
-        'HOST': '',                # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',       # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                # Set to empty string for default. Not used with sqlite3.
     }
 }
