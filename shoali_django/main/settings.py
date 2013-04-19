01e141286ef8cca9eddfac437c2c8386ec49f7d7
mUniKeS <munikes@members.fsf.org>
2013-04-19 13:54:12 +0200

    Add apps django-extensions, for class diagram

--- shoali_django/main/settings.py
+++ shoali_django/main/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'main.apps.core',
+    'django_extensions',
 )
 
 # A sample logging configuration. The only tangible logging
