d2ddb9a51216c34dee98c03d699c3852abe57f1b
mUniKeS <munikes@members.fsf.org>
2013-11-19 02:42:14 +0100

    new model Loan, change with Supply and Debt

--- shoali_django/main/apps/core/models.py
+++ shoali_django/main/apps/core/models.py
@@ -21,6 +21,9 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+logger = logging.getLogger(__name__)
+from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -83,60 +86,65 @@ class ShoaliUser (models.Model):
     def __unicode__(self):
         return self.nick
 
-    def create_shoaliuser (sender, instance, request, **kwargs):
+    def create_shoaliuser (self, sender, instance, request, **kwargs):
         ShoaliUser.objects.create(user=instance)
 
     user_registered.connect(create_shoaliuser, sender=User)
 
 
-class Supply (models.Model):
+class Loan (models.Model):
 
     UNIT_CHOICES = (
-            (0.00000001, 'sathosi (0.00000001)'),
-            (0.000001, 'μBTC (0.000001)'),
-            (0.001, 'mBTC (0.001)'),
-            (1, 'BTC (1)'),
-            (1000, 'kBTC (1000)'),
-            (1000000, 'MBTC (1000000)'),
+            (Decimal('0.00000001'), 'sathosi (0.00000001)'),
+            (Decimal('0.000001'), 'μBTC (0.000001)'),
+            (Decimal('0.001'), 'mBTC (0.001)'),
+            (Decimal('1'), 'BTC (1)'),
+            (Decimal('1000'), 'kBTC (1000)'),
+            (Decimal('1000000'), 'MBTC (1000000)'),
             )
-    users = models.ManyToManyField(ShoaliUser)
-    amount = models.PositiveIntegerField(verbose_name='Amount',
-            help_text='Indicate amount in .')
-    interest = models.PositiveIntegerField(verbose_name='Interest',
-            help_text='Indicate amount the interest of supply as %',
+    DAYS_CHOICES = (
+            (Decimal('1'), 'days'),
+            (Decimal('30'), 'month (30 days)'),
+            (Decimal('365'), 'years (365 days)'),
+            )
+    borrowers = models.ManyToManyField(ShoaliUser, related_name="borrowers",
             blank=True)
-    unit = models.CharField(max_length=13, choices=UNIT_CHOICES,
-            verbose_name='Unit')
+    lenders = models.ManyToManyField(ShoaliUser, related_name="lenders",
+            blank=True)
+    amount = models.DecimalField(verbose_name='Amount', max_digits=20, 
+            decimal_places=8, help_text='Indicate amount.')
+    interest = models.DecimalField(verbose_name='Interest', max_digits=6,
+            decimal_places=3, blank=True,
+            help_text='Indicate amount the interest of supply as %.')
+    unit = models.DecimalField(max_length=1, choices=UNIT_CHOICES,
+            max_digits=15, verbose_name='Unit', decimal_places=8)
     description = models.TextField (verbose_name='Description',
-            help_text='Describe why you want to incur in debt in 140 characters.',
+            help_text='Describe your main motivations.',
             blank=True)
+    period = models.PositiveIntegerField(verbose_name='Period',
+            help_text='Indicate repayment period.')
+    days = models.PositiveIntegerField(max_length=1,choices=DAYS_CHOICES, verbose_name='Days')
+
+    def _get_total_amount(self):
+        return  (self.amount * self.unit) * self.interest/Decimal('100') + (self.amount * self.unit)
+    total_amount = property(_get_total_amount)
+
+    def _get_total_days(self):
+        return self.period * self.days
+    total_days = property(_get_total_days)
+
+#    TODO: I don't know why not put this exception, problem with the m2m fields
+#    def save(self, *args, **kwargs):
+#        if self.lenders == 'blank' and self.borrowers == 'blank':
+#            raise Exception('It is possible that there is neither a loan \
+#lenders or borrowers')
+#        else:
+#            super(Loan, self).save(*args, **kwargs)
 
     def __unicode__(self):
-        return self.amount
-
-
-class Debt (models.Model):
-
-    UNIT_CHOICES = (
-            (0.00000001, 'sathosi (0.00000001)'),
-            (0.000001, 'μBTC (0.000001)'),
-            (0.001, 'mBTC (0.001)'),
-            (1, 'BTC (1)'),
-            (1000, 'kBTC (1000)'),
-            (1000000, 'MBTC (1000000)'),
-            )
-    users = models.ManyToManyField(ShoaliUser)
-    supply = models.OneToOneField(Supply)
-    amount = models.PositiveIntegerField(verbose_name='Amount',
-            help_text='Indicate amount in .')
-    unit = models.CharField(max_length=13, choices=UNIT_CHOICES,
-            verbose_name='Unit')
-    description = models.TextField (verbose_name='Description',
-            help_text='Describe why you want to incur in debt in 140 characters.',
-            blank=True)
-
-    def __unicode__(self):
-        return self.amount
+        return u'amount: %s %s - repayment period: %s %s - borrowers:[%s] and \
+lenders:[%s] - interest: %s %%' % (self.amount, self.unit, self.period, self.days,
+                    self.borrowers, self.lenders, self.interest)
 
 
 class Reputation (models.Model):
