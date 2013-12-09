547b7ff513445ffc6e0742ab6d55fa1abf71e70b
mUniKeS <munikes@members.fsf.org>
2013-12-09 02:00:14 +0100

    change function based view to class based view

--- shoali_django/main/apps/core/views.py
+++ shoali_django/main/apps/core/views.py
@@ -23,19 +23,144 @@
 
 import logging
 logger = logging.getLogger(__name__)
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.utils import simplejson as json
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.list import (BaseListView, ListView,
+                        MultipleObjectTemplateResponseMixin)
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateView
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ImproperlyConfigured
+
 
 from main.apps.core.models import BitcoinAddress, Loan
 from main.apps.core.forms import BitcoinAddressForm, LoanForm
 from main.apps.core.tasks import get_balance_and_progress_status
 
-def begin (request):
-    return render_to_response('index.html',{})
+
+class BeginView(TemplateView):
+    template_name = "index.html"
+
+
+class BitcoinAddressList(ListView):
+    model = BitcoinAddress
+
+
+class BitcoinAddressCreate(CreateView):
+    model = BitcoinAddress
+    form_class = BitcoinAddressForm
+    success_url = reverse_lazy('btc_list')
+
+    def get_initial(self):
+        return { "users": self.request.user.id }
+
+
+class BitcoinAddressUpdate(UpdateView):
+    model = BitcoinAddress
+    form_class = BitcoinAddressForm
+    success_url = reverse_lazy('btc_list')
+
+
+class BitcoinAddressDelete(DeleteView):
+    model = BitcoinAddress
+    success_url = reverse_lazy('btc_list')
+
+
+class DeletionMixin(object):
+    """
+    A mixin providing the ability to delete objects
+    """
+    success_url = None
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Calls the delete() method on the fetched objects and then
+        redirects to the success URL.
+        """
+        self.object_list = self.get_list_objects()
+        success_url = self.get_success_url()
+        for obj in self.object_list:
+            obj.delete()
+        return HttpResponseRedirect(success_url)
+
+    # Add support for browsers which only accept GET and POST for now.
+    def post(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
+
+    def get_success_url(self):
+        if self.success_url:
+            return self.success_url
+        else:
+            raise ImproperlyConfigured(
+                "No URL to redirect to. Provide a success_url.")
+
+
+class BaseDeleteListView(DeletionMixin, BaseListView):
+    """
+    Base view for deleting a list of objects.
+
+    Using this base class requires subclassing to provide a response mixin.
+    """
+
+
+class DeleteListView(MultipleObjectTemplateResponseMixin, BaseDeleteListView):
+     """
+     View for deleting a list of object retrieved with `self.get_list_objects()`,
+     with a response rendered by template.
+     """
+     template_name_suffix = '_confirm_list_delete'
+
+
+class BitcoinAddressDeleteList(DeleteListView):
+    model = BitcoinAddress
+    success_url = reverse_lazy('btc_list')
+    list_field_name = 'btc'
+
+
+class LoanList(ListView):
+    model = Loan
+
+
+class LendCreate(CreateView):
+    model = Loan
+    form_class = LoanForm
+    success_url = reverse_lazy('loan_list')
+
+    def get_initial(self):
+        return { "owner" : self.request.user,
+                 "lenders": [ self.request.user ] }
+
+
+class BorrowCreate(CreateView):
+    model = Loan
+    form_class = LoanForm
+    success_url = reverse_lazy('loan_list')
+
+    def get_initial(self):
+        return { "owner" : self.request.user,
+                 "borrowers": [ self.request.user ] }
+
+
+class LoanUpdate(UpdateView):
+    model = Loan
+    form_class = LoanForm
+    success_url = reverse_lazy('loan_list')
+
+
+class LoanDelete(DeleteView):
+    model = Loan
+    success_url = reverse_lazy('loan_list')
+
+
+class LoanDeleteList(DeleteListView):
+    model = Loan
+    success_url = reverse_lazy('loan_list')
+    list_field_name = 'loan'
+
 
 @login_required
 def user_info (request):
@@ -43,12 +168,17 @@ def user_info (request):
             context_instance=RequestContext(request))
 
 @login_required
-def user_btc_addresses (request):
+def user_btc_addresses (request, id=None):
     # init bitcoin address form
-    form_btc = BitcoinAddressForm ()
+    if id:
+        bitcoin_address = get_object_or_404(BitcoinAddress, pk=id)
+        form_btc = BitcoinAddressForm (instance=bitcoin_address)
+    else:
+        bitcoin_address = ''
+        form_btc = BitcoinAddressForm ()
     if request.method == 'POST':
-        form_btc = BitcoinAddressForm (request.POST)
         if request.POST.get('add'):
+            form_btc = BitcoinAddressForm (request.POST)
             if form_btc.is_valid():
                 # insert BTC address
                 # see if exists
@@ -63,40 +193,54 @@ def user_btc_addresses (request):
                     btc = BitcoinAddress.objects.get(
                             bitcoin_address=bitcoin_address)
                     btc.users.add(request.user.id)
-                logger.debug('insert BTC address: %s', bitcoin_address)
+                logger.debug('insert BTC address: %s for user %s',
+                        btc.bitcoin_address, request.user.username)
+        elif request.POST.get('save'):
+            form_btc = BitcoinAddressForm (request.POST, bitcoin_address)
+            if form_btc.is_valid():
+                # update BTC address
+                btc = form_btc.save()
+                logger.debug('update BTC address: %s for user %s',
+                        btc.bitcoin_address, request.user.username)
         elif request.POST.get('delete'):
-            try:
-                btc = BitcoinAddress.objects.get(
-                    bitcoin_address=request.POST.get('bitcoin_address'))
-                btc.users.remove(request.user.id)
-                if not btc.users.all():
-                    btc.delete()
+            btc_list = BitcoinAddress.objects.filter(id__in=request.POST.getlist('btc'))
+            for btc in btc_list:
                 logger.debug('delete BTC address: %s for user: %s',
-                    bitcoin_address, request.user.id)
-            except BitcoinAddress.DoesNotExist:
-                logger.debug('not delete BTC address because this BTC: %s not \
-exists for this user: %s', request.POST.get('bitcoin_address'), request.user.id)
+                        btc.bitcoin_address, request.user.username)
+            btc_list.delete()
+            #try:
+            #    btc = BitcoinAddress.objects.get(
+            #        bitcoin_address=request.POST.get('bitcoin_address'))
+            #    btc.users.remove(request.user.id)
+            #    if not btc.users.all():
+            #        btc.delete()
+            #    logger.debug('delete BTC address: %s for user: %s',
+            #        btc.bitcoin_address, request.user.username)
+            #except BitcoinAddress.DoesNotExist:
+            #    logger.debug('not delete BTC address because this BTC: %s not \
+#exists for this user: %s', request.POST.get('bitcoin_address'), 
+            #        request.user.username)
         return render_to_response('user/main.html',{'form_btc':form_btc},
                 context_instance=RequestContext(request))
     return render_to_response('user/main.html',
             {'form_btc':form_btc},context_instance=RequestContext(request))
+
 
 @login_required
 def do_loan(request):
     # init loan form
     form_loan = LoanForm()
     if request.method == 'POST':
-        form_loan = LoanForm(request.POST)
         if request.POST.get('add_lend') or request.POST.get('add_borrow'):
-            logger.debug('Create amount')
+            form_loan = LoanForm(request.POST)
             if form_loan.is_valid():
                 # insert the loan
                 loan = form_loan.save(commit=False)
                 loan.save()
                 if request.POST.get('add_lend'):
-                    loan.lenders.add(request.POST.get('lender'))
+                    loan.lenders.add(request.POST.get('lenders'))
                 elif request.POST.get('add_borrow'):
-                    loan.borrowers.add(request.POST.get('borrower'))
+                    loan.borrowers.add(request.POST.get('borrowers'))
                 logger.debug('Create amount: %s %s - repayment period: %s %s - \
 borrowers:%s and lenders:%s - interest: %s %%' % (loan.amount,
                     loan.get_unit_display(), loan.period,
@@ -117,6 +261,8 @@ borrowers:%s and lenders:%s - interest: %s %%' % (loan.amount,
             context_instance=RequestContext(request))
     return  render_to_response('user/main.html', {'form_loan': form_loan},
         context_instance=RequestContext(request))
+
+
 
 def getbalance (request):
     """
